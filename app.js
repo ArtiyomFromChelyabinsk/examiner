420c5f7361f6d62f0ebcd16b389e37e2a3d6021c
artiyom.moskalew.2@gmail.com <artiyom.moskalew.2@gmail.com>
2023-07-30 21:44:38 +0800

    add hello endpoint

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express"); 
 
- const app = express(); 
- const port = process.env.PORT || 3001; 
+const app = express(); 
+const port = process.env.PORT || 3001; 
  
- app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
    res.sendfile('index.html');
+ });
+ 
+app.get('/hello', (req, res) => {
+   res.send('Hello World, from express');
  });
   
  const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
