151f916594f96da8f6a0f304e21d9076f0e1edd6
artiyom.moskalew.2@gmail.com <artiyom.moskalew.2@gmail.com>
2023-07-30 20:52:11 +0800

    replace html

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express"); 
+
+
+
+//var app = express();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+
  const app = express(); 
  const port = process.env.PORT || 3001; 
   
- app.get("/", (req, res) => res.type('html').send(html)); 
+// app.get("/", (req, res) => res.type('html').send(html)); 
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+ });
   
  const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
   
