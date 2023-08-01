c6410895eaa2cea4c8448e7dc5a8c27e6185b21e
artiyom.moskalew.2@gmail.com <artiyom.moskalew.2@gmail.com>
2023-08-01 19:18:15 +0800

    u

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ res.json({ a: 1 });
  const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
   
  server.keepAliveTimeout = 120 * 1000; 
- server.headersTimeout = 120 * 1000; 
+ server.headersTimeout = 120 * 1000
  
  
  
