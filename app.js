452c015d3bb722903aa4712e7964d67f2692b999
artiyom.moskalew.2@gmail.com <artiyom.moskalew.2@gmail.com>
2023-08-03 05:03:55 +0800

    cp2

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ app.get("/", (req, res, next) => {
   res.json({ "message": "Ok" })
 });
 
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
 // Insert here other API endpoints
 
 // Default response for any other request
