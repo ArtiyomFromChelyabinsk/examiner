5cc245062f3a278b684efd40ea51e4e8dc00b46f
artiyom.moskalew.2@gmail.com <artiyom.moskalew.2@gmail.com>
2023-08-03 04:19:20 +0800

    print row

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/hello', (req, res) => {
 app.get('/db', (req, res) => {
   db.get("SELECT * FROM v", (error, row) => {
     // console.log('row=====' + row[0].id);
-    res.send('row.id');
+    res.send(row);
 });
 });
 
